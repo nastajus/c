fcfeef04a38d279808816f76c502acc7589c41e1
Ian Nastajus <ian.nastajus@gmail.com>
2018-03-23 19:06:00 -0400

    export / import experiment fizzle

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
+//SyntaxError: Unexpected token import
+//import {Model} from "./model";
+var model = require('model');
 var express = require('express');
 
 app = express();
 
-/*
-//Express 3.
-app.configure(function() {
-  app.use(express.static(__dirname + '/public'));
-});
-*/
-
-//Express 4
-//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/', function(request, response) {
-  response.send('hello world');
-});
+model.category = "asdf";
+model.date = "1q2qwe24";
 
 app.listen(3000, function () {
   console.log('example app is listening on port 3000');
---
+++ model.js
@@ -0,0 +1,12 @@
+export function Model () {
+
+	this.date = "";
+	this.category= "";
+	this.employeeName= "";
+	this.employeeAddress= "";
+	this.expenseDescription= "";
+	this.preTaxAmount= "";
+	this.taxName= "";
+	this.taxAmount= "";
+
+}
