e2d057e8a7e957c38a2ae2dab8eedb488547c9bb
Ian Nastajus <ian.nastajus@gmail.com>
2018-03-23 18:29:28 -0400

    html response

--- app.js
+++ app.js
@@ -2,6 +2,20 @@ var express = require('express');
 
 app = express();
 
+/*
+//Express 3.
+app.configure(function() {
+  app.use(express.static(__dirname + '/public'));
+});
+*/
+
+//Express 4
+//app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 app.get('/', function(request, response) {
   response.send('hello world');
 });
