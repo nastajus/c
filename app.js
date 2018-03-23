af296c31611dba30aebede813028aa365cf5f9d5
Ian Nastajus <ian.nastajus@gmail.com>
2018-03-23 19:29:29 -0400

    templating engine EJS working (disabled app.use(...))

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ var express = require('express');
 
 app = express();
 
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+  //res.sendFile(__dirname + '/public/index.html');
+  res.render('index', { posts: model.posts })
 });
 
 model.category = "asdf";
@@ -14,5 +17,4 @@ model.date = "1q2qwe24";
 
 app.listen(3000, function () {
   console.log('example app is listening on port 3000');
-  console.log(model);
 });
--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-	model: function() {
+	model : function() {
 		this.date = "";
 		this.category= "";
 		this.employeeName= "";
@@ -10,5 +10,38 @@ module.exports = {
 		this.taxName= "";
 		this.taxAmount= "";
 
-	}
+	},
+
+
+	// fake posts to simulate a database
+
+	//const posts = [
+	//posts = [
+	posts : [
+		{
+			id: 1,
+			author: 'John',
+			title: 'Templating with EJS',
+			body: 'Blog post number 1'
+		},
+		{
+			id: 2,
+			author: 'Drake',
+			title: 'Express: Starting from the Bottom',
+			body: 'Blog post number 2'
+		},
+		{
+			id: 3,
+			author: 'Emma',
+			title: 'Streams',
+			body: 'Blog post number 3'
+		},
+		{
+			id: 4,
+			author: 'Cody',
+			title: 'Events',
+			body: 'Blog post number 4'
+		}
+	]
+
 };
