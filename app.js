d5746f82d6e08f0144a55b7f35fec64c83595a8c
Ian Nastajus <ian.nastajus@gmail.com>
2018-03-23 21:31:24 -0400

    cleanup

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-var model = require('./model');
-var utils = require('./utils');
-var mock = require('./mock');
+var model = require('./js/model');
+var utils = require('./js/utils');
+var mock = require('./js/mock');
 var express = require('express');
 
 app = express();
@@ -8,32 +8,19 @@ app = express();
 app.set('view engine', 'ejs');
 
 
-
 var lorem_ipsum = mock.lorem_ipsum.split(' ');
 
-
 for (var i = 0; i < 4; i++) {
-  var e = new model.Employee();
-  e.employeeName = utils.random(lorem_ipsum);
-    //e.employeeName = lorem_ipsum[Math.floor(Math.random() * lorem_ipsum.length)];
-  e.employeeName = utils.capitalize(e.employeeName);
-    //e.employeeName ="ian";
-
-	//e.employeeName = utils.random(mock.addresses);
-  //e.address = mock.addresses[Math.floor(Math.random() * mock.addresses.length)]["street"];
-	e.employeeAddress = mock.addresses[Math.floor(Math.random() * mock.addresses.length)]["street"];
-  model.employees.push(e);
+	var e = new model.Employee();
+	e.employeeName = utils.random(lorem_ipsum);
+	e.employeeName = utils.capitalize(e.employeeName);
+	e.employeeAddress = utils.random(mock.addresses)["street"];
+	model.employees.push(e);
 }
 
-
 app.get('/', function(req, res) {
-  //res.render('index', { model: model });
 	res.render('index', { employees: model.employees });
 });
-
-
-// model.category = "asdf";
-// model.date = "1q2qwe24";
 
 app.listen(3000, function () {
   console.log('example app is listening on port 3000');
---
+++ js/model.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+	//model
+	Employee : function() {
+		this.date = "";
+		this.category= "";
+		this.employeeName= "";
+		this.employeeAddress= "";
+		this.expenseDescription= "";
+		this.preTaxAmount= "";
+		this.taxName= "";
+		this.taxAmount= "";
+	}
+	,
+
+	employees : []
+};
